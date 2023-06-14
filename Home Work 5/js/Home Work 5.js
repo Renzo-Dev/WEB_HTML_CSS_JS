db7905bac006a4e28760fb1df37e8087e97a75ef
Renzo-Dev <dankoshevoy2001@gmail.com>
2023-06-14 20:10:47 +0300

    update

--- Home Work 5/js/Home Work 5.js
+++ Home Work 5/js/Home Work 5.js
@@ -2,30 +2,60 @@
     window.addEventListener('load', function () {
         // поиска каждого элемента по совпадению идентификатора
         let pifTable = document.getElementById("myTable");
-        console.dir(pifTable);
         pifTable.style.borderCollapse = "collapse";
-
+                
         if (pifTable != null) {
             // получаем строки
             let rows = pifTable.getElementsByTagName('tr');
-
             for (let i = 0; i < rows.length; i++) {
                 // получаем все заголовки в таблице
-                let thcells = rows[i].getElementsByTagName('th');
-                for (let j = 0; j < thcells.length; j++) {
-                    thcells[j].style.border = '1px solid red';
-                    thcells[j].style.width = '50px';
-                    thcells[j].style.height = '50px'
+                let thCells = rows[i].getElementsByTagName('th');
+                for (let j = 0; j < thCells.length; j++) {
+                    thCells[j].classList.add("tableHead");
                     // задаем значения для заголовков
-                    if (i == 0) thcells[j].innerHTML = j;
-                    else thcells[j].innerHTML = i;
-                    thcells[j].classList.add("bgTable");
-                    thcells[j].classList.add("textColor")
+                    if (i == 0) thCells[j].innerHTML = j;
+                    else thCells[j].innerHTML = i;
+                    thCells[j].classList.add("bgTable");
+                    thCells[j].classList.add("textColor");
+                }
+            }
+
+            let tdCells = pifTable.querySelectorAll('td');
+
+            for (let i = 0; i < tdCells.length; i++) {
+                tdCells[i].classList.add("tableBox");
+                tdCells[i].classList.add("textColor");
+
+                //mouseout - генерится когда указатель мыши уходит с тела обьекта
+                tdCells[i].addEventListener('mouseout', function (e) {
+                    let tmpRow = e.target.parentNode.rowIndex * 10 - 10;
+                    let tmpCol = e.target.parentNode.rowIndex * 10 - 10 + e.target.cellIndex - 1;
+                    for (let s = tmpRow; s < tmpRow + e.target.cellIndex; s++) {
+                        tdCells[s].classList.remove('lighted');
+                    }
+                    for (let g = tmpCol; g > 0; g -= 10) {
+                        tdCells[g].classList.remove('lighted');
+                    }
+                });
+                tdCells[i].addEventListener('mouseover', function (e) {
+                    let tmpRow = e.target.parentNode.rowIndex * 10 - 10;
+                    let tmpCol = e.target.parentNode.rowIndex * 10 - 10 + e.target.cellIndex - 1;
+                    for (let s = tmpRow; s < tmpRow + e.target.cellIndex; s++) {
+                        tdCells[s].classList.add('lighted');
+                    }
+                    for (let g = tmpCol; g > 0; g -= 10) {
+                        tdCells[g].classList.add('lighted');
+                    }
+                });
+            }
+
+            let counter = 0;
+            for (let i = 1; i < 11; i++) {
+                for (let j = 1; j < 11; j++) {
+                    tdCells[counter].innerText = i * j;
+                    counter++;
                 }
             }
         }
-
-        tdCells
-
     })
 })()
